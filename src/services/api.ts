8ffd28a53bc488c81ca96547ac5b6305a53d3b5a
Tony Anderson <anderson.tony.98@gmail.com>
2026-01-03 23:15:59 -0300

    feat: implementa moderação de anúncios
    closes #15

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = "https://brincabem-api.tonyanderson.dev";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://brincabem-api.tonyanderson.dev";
 
 export const defaultHeaders = {
     "Content-Type": "application/json",
