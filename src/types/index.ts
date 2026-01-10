a666796d8df60cc08f614780406d991d49e64172
Tony Anderson <anderson.tony.98@gmail.com>
2026-01-10 12:42:48 -0300

    chore: define interfaces globais

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    role: 'user' | 'admin';
+}
+
+export interface Toy {
+    id: number;
+    title: string;
+    description: string;
+    category: string;
+    condition: 'new' | 'used';
+    status: 'pending' | 'available' | 'donated';
+    image_url: string;
+    gallery?: string[];
+    user_id: number;
+    created_at: string;
+}
+
+export interface AuthResponse {
+    token: string;
+    user: User;
+}
